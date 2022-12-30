bc8a332ad68c9cc702aa534f5599aefef3be702a
Alper <alper.demir.fener@gmail.com>
2022-12-30 20:58:30 +0300

    black & white added, scaling improved

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ window = tk.Tk()
 window.title("Image scaling")
 window.geometry("550x370")
 
-
 stringDifference = tk.StringVar()
 stringDifference.set("")
-labelDifferenceOfSize = tk.Label(window, textvariable=stringDifference, font="Arial 9")
+labelDifferenceOfSize = tk.Label(window, textvariable=stringDifference, font="Arial 9" ,padx=3, pady=3)
 labelDifferenceOfSize.place(x=320, y=130)
 
 targetHeight = tk.StringVar()
@@ -17,38 +16,44 @@ targetHeight.set("")
 labelHeight = tk.Label(window, textvariable=targetHeight, font="Arial 9", fg="black")
 labelHeight.place(x=128, y=100)
 
-
 stringInitialSize = tk.StringVar()
 stringInitialSize.set("")
 labelInitialSize = tk.Label(window, textvariable=stringInitialSize, font="Arial 9", fg="black")
 labelInitialSize.place(x=10, y=130)
-
 
 stringRenderedSize = tk.StringVar()
 stringRenderedSize.set("")
 labelRenderedSize = tk.Label(window, textvariable=stringRenderedSize, font="Arial 9", fg="black")
 labelRenderedSize.place(x=150, y=130)
 
-
 stringOriginalResolution = tk.StringVar()
 stringOriginalResolution.set("")
 labelStringOriginalResolution = tk.Label(window, textvariable=stringOriginalResolution, font="Arial 10", fg="black")
 labelStringOriginalResolution.place(x=120, y=42)
 
+stringMessage = tk.StringVar()
+stringMessage.set("")
+labelMessage = tk.Label(window, textvariable=stringMessage, font="Arial 10", fg="black",padx=5, pady=5)
+labelMessage.place(x=15, y=330)
+
 def selectFile():
 
     global filePath
     filePath = tk.filedialog.askopenfilename()
-    if(filePath !=''):
+    filename, file_extension = os.path.splitext(filePath)
+
+    if(filePath !='' and file_extension in [".jpg", ".jpeg", ".png"]):
         if(os.path.getsize(filePath) < 500000): # smaller image than 5 mb
             stringDifference.set("")
             labelDifferenceOfSize.__setitem__('bg','#F0F0F0')
             targetHeight.set("")
             stringInitialSize.set("")
             stringRenderedSize.set("")
+            stringMessage.set("")
+            labelMessage.__setitem__('bg', '#F0F0F0')
 
             image1 = Image.open(filePath)
-            new = image1.resize((250,146))
+            new = image1.resize((250, 146))
             test = ImageTk.PhotoImage(new)
 
             label1 = tk.Label(image=test)
@@ -67,16 +72,21 @@ def selectFile():
             path.place(x=120, y=12)
             print(image.size , "px")
 
-            stringOriginalResolution.set("Original Resolution:" + str(image.size) + "px")
-
+            stringOriginalResolution.set("Original Resolution:" + str(image.size[0]) + "x" + str(image.size[1]) + " px")
+            global widthh,height
+            widthh = image.size[0]
+            height = image.size[1]
         else:
-            print("Please give smaller image than 5mb")
+            labelMessage.__setitem__('bg', '#e0636f')
+            stringMessage.set("Warning: Please give smaller image than 5mb !")
+
     else:
-        print("Choose a file path")
+        labelMessage.__setitem__('bg','#e0636f')
+        stringMessage.set("Warning: Invalid file type only .jpeg, .png or .jpg allowed !")
+
 
 def calculateHeight(width):
-    # Based on 16:9 ratio
-    ratio = 1.777777777777778
+    ratio = widthh / height
     return width / ratio
 
 def calculateSizeDifference(initial,rendered):
@@ -90,8 +100,7 @@ def calculateSizeDifference(initial,rendered):
         stringDifference.set("File size increased by " + str(abs(round(formula, 2))) + "%")
         labelDifferenceOfSize.__setitem__('bg','#d94c6d')
 
-
-def Take_input():
+def Show():
     global INPUT,WIDTH
     INPUT = int(Output.get("1.0", "end-1c"))
     WIDTH = int(Width.get("1.0", "end-1c"))
@@ -102,14 +111,17 @@ def Take_input():
         targetHeight.set("Target Height(16:9):" + str(HEIGHT) + "px")
 
         new_image = image.resize((WIDTH, HEIGHT))
-        new_image.save('renderedImage.jpg', optimize=True, quality=INPUT)
+        if (varBlackAndWhite.get() == 1): # convert image to black & white
+            new_image = new_image.convert('L')
+
+        new_image.save('renderedImage.jpg', optimize=True, quality=INPUT) # save image as renderedImage
         renderedSize = os.path.getsize("renderedImage.jpg")
 
         stringRenderedSize.set("Rendered Size:" + str(renderedSize) + " Byte")
 
         calculateSizeDifference(initialSize, renderedSize)
-
         image1 = Image.open('renderedImage.jpg')
+
         new = image1.resize((250, 146))
         test = ImageTk.PhotoImage(new)
 
@@ -121,12 +133,21 @@ def Take_input():
     else:
         print("Please fill all inputs")
 
+def blackWhite():
+    if(varBlackAndWhite.get() == 1):
+        print("black and white mode ")
+    else:
+        print("color mode")
 
-b1 = tk.Button(text="Select File", font="Arial 12 bold", bg="#DCDCDC", fg="white", command=selectFile)
-b1.place(x=10, y=10)
+varBlackAndWhite = tk.IntVar() # if black & white selected keeps 1 else 0
+buttonBlackAndWhite = tk.Checkbutton(window, text='Black & White',variable=varBlackAndWhite, onvalue=1, offvalue=0, command=blackWhite)
+buttonBlackAndWhite.place(x=130,y=68)
 
-l = tk.Label(text="Quality(%):")
-l.place(x=10, y=70)
+SelectFileButton = tk.Button(text="Select File", font="Arial 12 bold", bg="#DCDCDC", fg="white", command=selectFile)
+SelectFileButton.place(x=10, y=10)
+
+labelQuality = tk.Label(text="Quality(%):")
+labelQuality.place(x=10, y=70)
 
 inputWidth = tk.Label(text="Width(px):")
 inputWidth.place(x=10, y=100)
@@ -137,8 +158,9 @@ Output.place(x=75, y=70)
 Width = tk.Text(window, height=1, width=5, bg="#F6F8FA")
 Width.place(x=75, y=100)
 
-Display = tk.Button(window, height=2, width=10, text ="Show", command=lambda: Take_input())
+Display = tk.Button(window, height=2, width=10, text ="Show", command=lambda: Show())
 
 Display.place(x=470, y=320, width=70)
 
+window.resizable(False,False)
 window.mainloop()
